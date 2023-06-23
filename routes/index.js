e33b1fc1ea454502780fc97b03cc8eaff2afb60b
v.terzian <v.terzian@groupeonepoint.com>
2023-06-23 20:51:21 +0200

    replace all var by const and replace function by arrow function

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const usersRoute = require('./users');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('ok');
 });
 
