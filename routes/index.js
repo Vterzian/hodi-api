f125b9471aef0c36b73eec2c6b2938845de69a71
v.terzian <v.terzian@groupeonepoint.com>
2023-06-23 20:35:23 +0200

    install express js

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+const usersRoute = require('./users');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('ok');
+});
+
+router.use('/users', usersRoute);
+
+module.exports = router;
