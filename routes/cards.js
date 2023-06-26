7812b40ec0524b9853f7ebae2dc642a0d8687773
vterzian <valterzian@gmail.com>
2023-06-26 19:54:51 +0200

    (feature/basic-error-handling): add basic error handler

--- routes/cards.js
+++ routes/cards.js
@@ -4,64 +4,103 @@ const { PrismaClient } = require('@prisma/client');
 const router = express.Router();
 const prisma = new PrismaClient()
 
+
+// TODO find a better way to check if card exist before updating or deleting
+router.all('/:id', async (req, res, next) => {
+  try {
+    if (req.method !== "GET" || req?.params?.id) {
+      await prisma.card.findUniqueOrThrow({
+        where: {
+          id: parseInt(req.params.id)
+        }
+      });
+    }
+
+    next();
+  } catch(error) {
+    next(error);
+  }
+});
+
 /* GET cards listing. */
 router.get('/', async (req, res, next) => {
-  const cardList = await prisma.card.findMany();
+  try {
+    const cardList = await prisma.card.findMany();
 
-  res.json(cardList);
+    res.json(cardList);
+  } catch(error) {
+    next(error);
+  }
 });
 
 /* GET specific card */
 router.get('/:id', async (req, res, next) => {
-  const card = await prisma.card.findUnique({
-    where: {
-      id: parseInt(req.params.id)
-    }
-  });
+  try {
+    const card = await prisma.card.findUniqueOrThrow({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    });
 
-  res.json(card);
+    res.json(card);
+  } catch(error) {
+    next(error);
+  }
 });
 
 /* ADD new card */
 router.post('/', async (req, res, next) => {
-  const { name, attack, health } = req.body;
-  const newCard = await prisma.card.create({
-    data: {
-      name,
-      attack,
-      health,
-    },
-  });
+  try {
+    const { name, attack, health } = req.body;
 
-  res.json(newCard);
+    const newCard = await prisma.card.create({
+      data: {
+        name,
+        attack,
+        health,
+      },
+    });
+
+    res.status(201).json(newCard);
+  } catch(error) {
+    next(error);
+  }
 });
 
 /* UPDATE a specific card */
 router.put('/:id', async (req, res, next) => {
-  const { name, attack, health } = req.body;
-  const updatedCard = await prisma.card.update({
-    data: {
-      name,
-      attack,
-      health,
-    },
-    where: {
-      id: parseInt(req.params.id)
-    }
-  });
+  try {
+    const { name, attack, health } = req.body;
+    const updatedCard = await prisma.card.update({
+      data: {
+        name,
+        attack,
+        health,
+      },
+      where: {
+        id: parseInt(req.params.id)
+      }
+    });
 
-  res.json(updatedCard);
+    res.json(updatedCard);
+  } catch(error) {
+    next(error);
+  }
 });
 
 /* DELETE specific card */
-router.delete('/:id', async (req, res) => {
-  const card = await prisma.card.delete({
-    where: {
-      id: parseInt(req.params.id)
-    }
-  });
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const card = await prisma.card.delete({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    });
 
-  res.json('ok');
+    res.status(204).json('ok');
+  } catch(error) {
+    next(error);
+  }
 });
 
 
