f24ad3c1d143593e2958e82650598186e66b38de
vterzian <valterzian@gmail.com>
2023-07-02 01:00:11 +0200

    (feature/controller-separation): separate route and controller

--- routes/cards.js
+++ routes/cards.js
@@ -1,92 +1,20 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
 const { getOneCardSchema, addCardSchema, updateCardSchema } = require('../schemas/card');
-const { validateSchema } = require('../middleware/validationMiddleware');
+const { validateMiddleware } = require('../middleware/validationMiddleware');
+const { 
+  getCardListController, 
+  getOneCardController, 
+  addCardController, 
+  updateCardController, 
+  deleteCardController 
+} = require('../controllers/cardController');
 
 const router = express.Router();
-const prisma = new PrismaClient()
 
-/* GET cards listing. */
-router.get('/cards', async (req, res, next) => {
-  try {
-    const cardList = await prisma.card.findMany();
-
-    res.json(cardList);
-  } catch(error) {
-    next(error);
-  }
-});
-
-/* GET specific card */
-router.get('/cards/:id', validateSchema(getOneCardSchema), async (req, res, next) => {
-  try {
-    const card = await prisma.card.findUniqueOrThrow({
-      where: {
-        id: parseInt(req.params.id)
-      }
-    });
-
-    res.json(card);
-  } catch(error) {
-    next(error);
-  }
-});
-
-/* ADD new card */
-router.post('/cards', validateSchema(addCardSchema), async (req, res, next) => {
-  try {
-    const { name, attack, health } = req.body;
-
-    const newCard = await prisma.card.create({
-      data: {
-        name,
-        attack,
-        health,
-      },
-    });
-
-    res.status(201).json(newCard);
-  } catch(error) {
-    next(error);
-  }
-});
-
-/* UPDATE a specific card */
-router.put('/cards/:id', validateSchema(updateCardSchema), async (req, res, next) => {
-  try {
-    await prisma.card.findUniqueOrThrow({
-      where: {
-        id: parseInt(req.params.id)
-      }
-    });
-  
-    const updatedCard = await prisma.card.update({
-      data: req.body,
-      where: {
-        id: parseInt(req.params.id)
-      }
-    });
-
-    res.json(updatedCard);
-  } catch(error) {
-    next(error);
-  }
-});
-
-/* DELETE specific card */
-router.delete('/cards/:id', validateSchema(getOneCardSchema), async (req, res, next) => {
-  try {
-    const card = await prisma.card.delete({
-      where: {
-        id: parseInt(req.params.id)
-      }
-    });
-
-    res.status(204).json('ok');
-  } catch(error) {
-    next(error);
-  }
-});
-
+router.get('/cards', getCardListController);
+router.get('/cards/:id', validateMiddleware(getOneCardSchema), getOneCardController);
+router.post('/cards', validateMiddleware(addCardSchema), addCardController);
+router.put('/cards/:id', validateMiddleware(updateCardSchema), updateCardController);
+router.delete('/cards/:id', validateMiddleware(getOneCardSchema), deleteCardController);
 
 module.exports = router;
